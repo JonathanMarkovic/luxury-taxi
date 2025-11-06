536840f83ea2ac536f8e2c368d41fa63a6bd3809
JonathanMarkovic <jonathan.markovic@outlook.com>
2025-11-06 16:31:58 -0500

    Edits to controllers

--- app/Controllers/CarsController.php
+++ app/Controllers/CarsController.php
@@ -3,25 +3,27 @@
 namespace App\Controllers;
 
 use App\Controllers\BaseController;
+use App\Domain\Models\CarModel;
 use DI\Container;
 use Psr\Http\Message\ResponseInterface as Response;
 use Psr\Http\Message\ServerRequestInterface as Request;
 
-class CarsController extends BaseController {
-    public function __construct(Container $container)
+class CarsController extends BaseController
+{
+    public function __construct(Container $container, private CarModel $car_model)
     {
         parent::__construct($container);
     }
 
     public function index(Request $request, Response $response, array $args): Response
     {
+        $cars = $this->car_model->fetchCars();
         $data['data'] = [
             'title' => 'Admin',
             'message' => 'Welcome to the admin page',
+            'cars' => $cars
         ];
 
-        return $this-render($response,
-        //!Enter the view file name here ,
-          $data);
+        return $this->render($response, 'admin/cars/carIndexView.php', $data);
     }
 }
--- app/Controllers/PaymentController.php
+++ app/Controllers/PaymentController.php
@@ -3,25 +3,31 @@
 namespace App\Controllers;
 
 use App\Controllers\BaseController;
+use App\Domain\Models\PaymentModel;
 use DI\Container;
 use Psr\Http\Message\ResponseInterface as Response;
 use Psr\Http\Message\ServerRequestInterface as Request;
 
-class PaymentController extends BaseController {
-    public function __construct(Container $container)
+class PaymentController extends BaseController
+{
+    public function __construct(Container $container, private PaymentModel $payment_model)
     {
         parent::__construct($container);
     }
 
     public function index(Request $request, Response $response, array $args): Response
     {
+        $payment = $this->payment_model->fetchPaymentByID();
         $data['data'] = [
             'title' => 'Admin',
             'message' => 'Welcome to the admin page',
+            'payment' => $payment
         ];
 
-        return $this-render($response,
-        //!Enter the view file name here ,
-          $data);
+        return $this - render(
+            $response,
+            //!Enter the view file name here ,
+            $data
+        );
     }
 }
