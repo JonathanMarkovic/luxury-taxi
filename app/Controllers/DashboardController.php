e82df1d1ca69335a2d9b0a979a21324d33aae5f9
JonathanMarkovic <jonathan.markovic@outlook.com>
2025-11-06 14:41:35 -0500

    Created Views
    Created all Admin View files

--- app/Controllers/DashboardController.php
+++ app/Controllers/DashboardController.php
@@ -7,12 +7,21 @@ use DI\Container;
 use Psr\Http\Message\ResponseInterface as Response;
 use Psr\Http\Message\ServerRequestInterface as Request;
 
-class DashboardController extends BaseController {
+class DashboardController extends BaseController
+{
     public function __construct(Container $container)
     {
         parent::__construct($container);
     }
 
+    /**
+     * Summary of index
+     * The index page for the admin dashboard.
+     * @param \Psr\Http\Message\ServerRequestInterface $request
+     * @param \Psr\Http\Message\ResponseInterface $response
+     * @param array $args
+     * @return float|int
+     */
     public function index(Request $request, Response $response, array $args): Response
     {
         $data['data'] = [
@@ -20,8 +29,10 @@ class DashboardController extends BaseController {
             'message' => 'Welcome to the admin page',
         ];
 
-        return $this-render($response,
-        //!Enter the view file name here ,
-          $data);
+        return $this - render(
+            $response,
+            //!Enter the view file name here ,
+            $data
+        );
     }
 }
