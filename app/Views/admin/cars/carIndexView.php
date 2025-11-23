f16b13598d6af2bce5f6694900eed502277691a3
taliamuro <taliamuro3@gmail.com>
2025-11-23 18:10:08 -0500

    Added navigation button to cars index view

--- app/Views/admin/cars/carIndexView.php
+++ app/Views/admin/cars/carIndexView.php
@@ -3,49 +3,38 @@
 use App\Helpers\ViewHelper;
 //TODO: set the page title dynamically based on the view being rendered in the controller.
 $page_title = 'Home';
-ViewHelper::loadHeader($page_title);
+ViewHelper::loadAdminHeader($page_title);
 $cars = $data['cars'];
+
 ?>
 
 <main class="col-md-9 ms-sm-auto col-lg-10 px-md-4">
-    <div
-        class="d-flex justify-content-between flex-wrap flex-md-nowrap align-items-center pt-3 pb-2 mb-3 border-bottom">
+    <div class="d-flex justify-content-between flex-wrap flex-md-nowrap align-items-center pt-3 pb-2 mb-3 border-bottom">
         <h1 class="h2">Dashboard</h1>
         <div class="btn-toolbar mb-2 mb-md-0">
-            <div class="btn-group me-2">
-                <button type="button" class="btn btn-sm btn-outline-secondary">
-                    Share
-                </button>
-                <button type="button" class="btn btn-sm btn-outline-secondary">
-                    Export
-                </button>
+            <div>
+                <!--ADD this to route and redirect to faqCreateView.php -->
+                <a class="btn btn-primary" href="<?= APP_ADMIN_URL ?>/cars/create">Create New Car</a>
             </div>
-            <button
-                type="button"
-                class="btn btn-sm btn-outline-secondary dropdown-toggle d-flex align-items-center gap-1">
-                <svg class="bi" aria-hidden="true">
-                    <use xlink:href="#calendar3"></use>
-                </svg>
-                This week
-            </button>
         </div>
     </div>
     <!-- Canvas is the graph that would show on the main page -->
     <!-- <canvas
     <h2>Products List</h2>
-        <!-- Table of products should go here -->
+        <-- Table of products should go here -->
     <table class="table table-striped table-hover">
         <thead>
             <tr>
-                <td>car id</td>
-                <td>brand</td>
-                <td>model</td>
-                <td>year</td>
-                <td>capacity</td>
-                <td>approx_price</td>
-                <td>description</td>
-                <td>created at</td>
-                <td>updated at</td>
+                <td>ID</td>
+                <td>Brand</td>
+                <td>Model</td>
+                <td>Year</td>
+                <td>Capacity</td>
+                <td>Approximate Price</td>
+                <td>Description</td>
+                <td>Created at</td>
+                <td>Updated at</td>
+                <td>Actions</td>
             </tr>
         </thead>
         <tbody>
